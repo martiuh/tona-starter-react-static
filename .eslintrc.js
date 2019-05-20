ff573c8cdd8cc4e6597d0dea4cdfa6fd5fe0416f
Martiuh <martiuh@gmail.com>
2019-05-20 12:55:25 -0500

    fix: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: 'react-tools',
   plugins: [
     'react-hooks'
-  ]
+  ],
   rules: {
     'jsx-a11y/label-has-for': 0,
     'jsx-a11y/label-has-associated-control': 1,
@@ -10,7 +10,7 @@ module.exports = {
       components: ['Link'],
       specialLink: ['to']
     }],
-    // "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
-    // "react-hooks/exhaustive-deps": "warn" // Checks effect dependencies  
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "warn" // Checks effect dependencies  
   }
 }
